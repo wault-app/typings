8f3cf16e3fe808a41615719b17cae52453c3afc7
Gál Péter <pepyta118@gmail.com>
2021-09-24 09:25:20 +0200

    chore: remove session token typings

--- dist/src/SessionToken.d.ts
+++
@@ -1,6 +0,0 @@
-import { DeviceType } from "./Device";
-import { UserType } from "./User";
-export declare type SessionTokenType = UserType & {
-    device: DeviceType;
-};
-//# sourceMappingURL=SessionToken.d.ts.map
